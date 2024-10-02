59a0b76d57fa7843c3849158b49b3c9fd09934fe
Mohd Faiz <faiz2929ansari@gmail.com>
2024-10-02 18:14:22 +0530

    second commit

--- server.js
+++ server.js
@@ -1,8 +1,35 @@
-import app from "./app.js";
-import dotenv from 'dotenv';
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import { errorMiddleware } from "./middlewares/error.js";
+import reservationRouter from "./routes/reservationRoute.js";
+import { dbConnection } from "./database/dbConnection.js";
+
+const app = express();
 dotenv.config();
 
+app.use(
+  cors({
+    origin: 'http://localhost:5173',
+    methods: ["POST"],
+    credentials: true,
+  })
+);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/reservation", reservationRouter);
+app.get("/", (req, res, next)=>{return res.status(200).json({
+  success: true,
+  message: "HELLO WORLD AGAIN 2"
+})})
+
+dbConnection();
+
+app.use(errorMiddleware);
+
 app.listen(process.env.PORT, ()=>{
-    console.log(`SERVER HAS STARTED AT PORT ${process.env.PORT}`);
+  console.log(`SERVER HAS STARTED AT PORT ${process.env.PORT}`);
 })
-         
+
+// export default app;
